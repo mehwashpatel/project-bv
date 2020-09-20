08ab4010e30e568b55f8b410a90f5a3c207f3807
Mehwash <patel_mehwash@yahoo.co.in>
2020-09-20 19:34:16 -0400

    BootstrapVue Application

---
+++ src/components/custom-header.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item href="/">Home</b-nav-item>
+            <b-nav-item href="/about">About</b-nav-item>
+          </b-navbar-nav>
+
+        </b-collapse>
+      </b-navbar>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+    #nav a {
+      color: white;
+    }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+     <b-container>
+        <b-row>
+          <custom-card v-for="index in 3" :key="index"></custom-card>
+        </b-row>
+     </b-container>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import CustomCard from "@/components/custom-card.vue";
+
+export default {
+  name: 'Home',
+  components: { "custom-card": CustomCard },
+}
+</script>
